ab736177b37bdbebdb69aec77efbb6add5f4fbc7
Simon <simon.mayer@lego.com>
2024-10-19 14:12:01 +0200

    api key update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/WeatherAPI/',
+  base: "/WeatherAPI/",
   plugins: [vue()],
+  define: {
+    "process.env": process.env,
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
-  envDir: 'src',
-})
+  envDir: ".",
+});
